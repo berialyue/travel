98a0568b9204a7137eba93b8ce4f52ad5521cf4a
berialyue <berialyue@gmail.com>
2018-06-06 09:36:35 +0800

    add swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="Home">
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './compoents/Header';
+import HomeSwiper from './compoents/Swiper';
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
+    HomeSwiper,
   },
 };
 </script>
---
+++ src/pages/home/compoents/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1703/c1/681faf4fa8ae6e02.jpg_750x200_66a2f930.jpg',
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/fe/ca8ed7adc3ff9f02.jpg_750x200_49735140.jpg',
+      }],
+    };
+  },
+  // computed: {
+  //   swiper() {
+  //     return this.$refs.mySwiper.swiper;
+  //   },
+  // },
+  // mounted() {
+  //   // current swiper instance
+  //   // 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
+  //   this.swiper.slideTo(3, 1000, false);
+  // },
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: white
+.wrapper
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  .swiper-img
+    width: 100%;
+    background: #eee;
+</style>
+
