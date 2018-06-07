39ec5ac5c756ae3e50fe3f8cf486ff509d3d5f34
berialyue <berialyue@gmail.com>
2018-06-07 22:36:41 +0800

    add list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,47 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list :presentcity="presentcity" :hotcitys="hotcitys"></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import CityHeader from './components/Header';
 import CitySearch from './components/Search';
+import CityList from './components/List';
+import CityAlphabet from './components/Alphabet';
 
 export default {
   name: 'City',
+  data() {
+    return {
+      presentcity: '',
+      hotcitys: [],
+    };
+  },
   components: {
     CityHeader,
     CitySearch,
+    CityList,
+    CityAlphabet,
+  },
+  methods: {
+    getCityInfo() {
+      axios.get('/api/city.json')
+        .then(this.getCityInfoSucc);
+    },
+    getCityInfoSucc(res) {
+      if (res.data.ret && res.data.data) {
+        const data = res.data.data;
+        this.presentcity = data.presentcity;
+        this.hotcitys = data.hotcitys;
+      }
+    },
+  },
+  mounted() {
+    this.getCityInfo();
   },
 };
 </script>
